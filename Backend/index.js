b48d2c9722e41387ef7324709a8db37a9f803140
pallavi <pallavi1782003@gmail.com>
2025-10-25 21:23:53 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -36,7 +36,7 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 app.use(express.static(path.join(_dirname,"../frontend/dist")));
-app.get('*', (req, res) => {
+app.get('/*splat', (_,res) => { 
 
    res.sendFile(path.resolve(_dirname,"../frontend","dist","index.html"));
 
