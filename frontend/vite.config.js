390da8d4b0f31f347153e6ecb75c1952aae7fc08
pallavi <pallavi1782003@gmail.com>
2025-10-26 16:13:06 +0530

    port change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/JobPortal/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
